e6b5d108c0901de5c5f64feca5b06ae47b0a7f61
ivan-starcevic <108392306+ivan-starcevic@users.noreply.github.com>
2025-08-28 15:04:33 +0200

    chore: remove IconMenuWithBadge from main demo

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import CssBaseline from "@mui/material/CssBaseline";
 import Container from "@mui/material/Container";
-import IconMenuWithBadge from "./IconMenuWithBadge.jsx";
 import ResponsiveTester from "./ResponsiveTester.jsx";
 import PageHeading from "./PageHeading.jsx";
 
@@ -10,7 +9,6 @@ createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <CssBaseline />
     <Container sx={{ p: 4, display: 'grid', gap: 4 }}>
-      <IconMenuWithBadge />
 
       <ResponsiveTester initialWidth={900} minWidth={320}>
         <PageHeading
